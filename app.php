fb598c981357585cf7b83d9c07ddf3626234bc2c
azanette <alexandreczanette@gmail.com>
2017-10-16 10:32:24 -0200

    Update app.php

--- app.php
+++ app.php
@@ -9,6 +9,17 @@ $app['debug'] = false;
 
 /* Criando os serviços com o Pimple */
 $app['url_site'] = 'http://www.meusite.com';
+$app['view.config'] = [
+	'path_templates' => __DIR__ . '/../templates'
+];
+$app['view.renderer'] = function() use($app){
+	$pathTemplates = $app['view.config']['path_templates'];
+	return new ViewRenderer($pathTemplates);
+};
 
+/* Enviado conteúdo para url.php */
+$site->get('/url', function (Request $request) use ($app) {
+    return $app['view.renderer']->render('url');
+});
 
 $app->run();
