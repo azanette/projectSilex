10719044554650f84787ae8c1dea69fcf6161e43
azanette <alexandreczanette@gmail.com>
2017-10-16 10:34:29 -0200

    Update ViewRenderer.php

--- src/View/ViewRenderer.php
+++ src/View/ViewRenderer.php
@@ -1,1 +1,27 @@
+<?php 
+namespace SON\View;
 
+class ViewRenderer{
+
+	private $pathTemplates;
+	private $templateName;
+
+	/**
+	 * Class Constructor
+	 * @param    $pathTemplates   
+	 */
+	public function __construct($pathTemplates)
+	{
+		$this->pathTemplates = $pathTemplates;
+	}
+
+	public function render($name, array $data = []){
+		$this->templateName = $name;
+		extract($data); //['name' => 'valor qualquer']; $name = 'valor qualquer'
+		ob_start();
+		include $this->pathTemplates . '/'.$this->templateName.'.php';
+		$output = ob_get_clean();
+		return $output;			
+	}
+  
+}
